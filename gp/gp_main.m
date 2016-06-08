438010b3317e0f3a743e8981adc2fd369b0c43a4
James Guevara <jag2231@columbia.edu>
2016-06-08 12:06:23 -0400

    Add files via upload

--- gp/gp_main.m
+++ gp/gp_main.m
@@ -10,11 +10,41 @@ gpParameters.lik = gpLik;
 
 predictionLocs = [];
 counter = 1;
-for i=50:10:520
-    for j=50:10:400
+for i=10:10:160
+    for j=10:10:160
         predictionLocs(counter,:) = [i j];
         counter = counter + 1;
     end
 end
 
 [locMeans, locCovs] = computePredictions(gpParameters, trackletData, predictionLocs);
+
+plotVectorFields(imageStack,predictionLocs,locMeans,trackletData);
+
+%% S2 parameters
+gpCov = [1.1176; -1.1311];
+gpLik = -7.7531;
+gpParameters.cov = gpCov;
+gpParameters.lik = gpLik;
+
+predictionLocs = [];
+counter = 1;
+for i=10:10:160
+    for j=10:10:160
+        predictionLocs(counter,:) = [i j];
+        counter = counter + 1;
+    end
+end
+
+[locMeans, locCovs] = computePredictions(gpParameters, trackletData, predictionLocs);
+
+plotVectorFields(imageStack,predictionLocs,locMeans,trackletData);
+
+
+
+
+
+
+
+
+
