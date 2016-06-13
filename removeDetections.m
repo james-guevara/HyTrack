b364c285a69fa2ae7065d5570b5f7fbf6ebc52b8
frankzhu0 <fz2214@columbia.edu>
2016-06-13 19:45:01 -0400

    Add files via upload

---
+++ removeDetections.m
@@ -0,0 +1,19 @@
+function newDetections = removeDetections( detections, rows, cols )
+N = size(detections,1);
+newDetections = [];
+if (N == 0)
+    return;
+end
+counter = 1;
+for n=1:N
+    if (detections(n,1) < cols-5 && detections(n,1) > 5 ...
+            && detections(n,2) < rows-5 && detections(n,2) > 5)
+        newDetections(counter,1) = detections(n,1);
+        newDetections(counter,2) = detections(n,2);
+        newDetections(counter,3) = detections(n,3);
+        counter = counter + 1;
+    end
+end
+
+end
+
