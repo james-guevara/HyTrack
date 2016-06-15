66f76519c67b09f4144905d4c67e76c5f34fbf02
James Guevara <jag2231@columbia.edu>
2016-06-15 16:24:54 -0400

    Add files via upload

--- tracklet_association/propagateState.m
+++ tracklet_association/propagateState.m
@@ -1,13 +1,18 @@
-function [ finalState, stateCovariance ] = propagateState( initialState, initialTime, numTimes, gpParameters, trackletData)
+function [ finalState, stateCovariance, states ] = propagateState( initialState, initialTime, numTimes, gpParameters, trackletData)
 % initialState corresponds to tail of an initial tracklet.
 % numTimes is how many times you want to propagate the state into the
 % future.
 temporaryState = initialState;
 stateCovariance = [1 1];
+states = [];
+states(1,:) = initialState;
+counter = 2;
 for t=initialTime:initialTime+numTimes-1
     [velocityMean, velocityCovariance] = computePrediction(temporaryState,t,gpParameters,trackletData);
     temporaryState = temporaryState + velocityMean;
     stateCovariance = stateCovariance + velocityCovariance;
+    states(counter,:) = temporaryState;
+    counter = counter + 1;
 end
 finalState = temporaryState;
 
